d024296878dd937b1a893b34998fb8836e66cb4d
leeboh23 <leeboh23@gmail.com>
2021-01-26 13:18:03 +0900

    greedy

---
+++ greedy/최고의 피자.py
@@ -0,0 +1,31 @@
+﻿'''
+https://codeup.kr/problem.php?id=3321
+'''
+
+n = int(input())
+a, b = map(int, input().split())
+c = int(input())
+d = []
+for i in range(n) : 
+  d.append(int(input()))
+
+d.sort(reverse=True)
+
+#첫 칼로리는 도우 칼로리로
+cal = c
+
+tmp = float(cal / a) #각 순간의 칼로리값
+mx = tmp #그 전의 칼로리값
+
+#1달러 당 칼로리가 낮아지기 전까지 계속 내림차순으로 더함
+for i in range(n) :
+  cal = cal + d[i]
+  tmp = float(cal / (a + b * (i + 1)))
+
+  if tmp < mx : 
+    break
+  else :
+    mx = tmp
+
+print(int(mx))
+
---
+++ greedy/펜션 - 시간초과.py
@@ -0,0 +1,42 @@
+﻿'''
+https://codeup.kr/problem.php?id=4040
+'''
+'''
+도착하는날~(출발하는날-1)만큼 방을 쓸 수 있어야 함
+'''
+n, m = map(int, input().split())
+
+d = []
+for i in range(n) : 
+  d.append(list(input()))
+
+s, t = map(int, input().split())
+
+str_list = list(map(list, zip(*d)))
+
+mx = [0, 0]
+
+'''
+a = 묵은 방의 수
+b = 날짜 계산용
+'''
+a = 0
+b = t - s
+
+
+while b > 0 : 
+  cnt = [0 for _ in range(m)]
+
+  #연속되는 방이 제일 많은 거 찾기
+  for i in range(m) : 
+    day = (t - b) - 1 #날짜를 인덱스로
+    if str_list[i][day] == 'O' : 
+      cnt[i] = 1
+      while str_list[i][day+1] == 'O' :
+        cnt[i] = cnt[i] + 1
+        day = day + 1
+
+  b = b - max(cnt)
+  a = a + 1
+
+print(a-1) #방 갯수를 옮긴 횟수로 환산
